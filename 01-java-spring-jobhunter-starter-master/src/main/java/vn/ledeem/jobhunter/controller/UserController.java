06dc976a1c48b3916eb23eef98e23587bfb6388c
LeThanh052 <thanhle050204@gmail.com>
2025-10-06 20:37:18 +0700

    crud done

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/UserController.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/UserController.java
@@ -5,7 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.ledeem.jobhunter.domain.User;
 import vn.ledeem.jobhunter.service.UserService;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -16,15 +23,39 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
-        User user = new User();
-        user.setEmail("thanh@gmail.com");
-        user.setName("thanh dep trai");
-        user.setPassword("4303203");
+    // @GetMapping("/user/create")
+    @PostMapping("/user")
+    public User createNewUser(
+            @RequestBody User postManUser) {
+        User deUser = this.userService.handleCreateUser(postManUser);
+        return deUser;
+    }
 
-        this.userService.handleCreateUser(user);
-        return "Hello Viet Nam";
+    // delete user by id
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        this.userService.handleDeleteUser(id);
+        return "delete user";
+    }
+
+    // fetch user by id
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
+        return this.userService.fetchUserById(id);
+    }
+
+    // fetch all users
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        return this.userService.fetchAllUsers();
+    }
+
+    // update user by id
+    @PutMapping("/user")
+    public User updateNewUser(
+            @RequestBody User user) {
+        User deUser = this.userService.handleUpdateUser(user);
+        return deUser;
     }
 
 }
--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/service/UserService.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.ledeem.jobhunter.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import vn.ledeem.jobhunter.domain.User;
 
@@ -13,7 +15,38 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void handleCreateUser(User user) {
-        this.userRepository.save(user);
+    public User handleCreateUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(Long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    // public User fetchUserById(long id) {
+    // Optional<User> userOptional = this.userRepository.findById(id);
+    // if (userOptional.isPresent()) {
+    // return userOptional.get();
+    // }
+    // return null;
+    // }
+    public User fetchUserById(Long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> fetchAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User reqUser) {
+        User currentUser = this.fetchUserById(reqUser.getId());
+        if (currentUser != null) {
+            currentUser.setName(reqUser.getName());
+            currentUser.setEmail(reqUser.getEmail());
+            currentUser.setPassword(reqUser.getPassword());
+            // update user
+            return this.userRepository.save(currentUser);
+        }
+        return currentUser;
     }
 }
