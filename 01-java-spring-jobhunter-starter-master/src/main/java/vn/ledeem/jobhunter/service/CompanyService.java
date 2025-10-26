fd66501e1667a2719fba965b0e3ffc7b8996399b
LeThanh052 <thanhle050204@gmail.com>
2025-10-26 22:59:42 +0700

    GetPutDelete Company

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/service/CompanyService.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/service/CompanyService.java
@@ -1,8 +1,12 @@
 package vn.ledeem.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.ledeem.jobhunter.domain.Company;
+import vn.ledeem.jobhunter.domain.User;
 import vn.ledeem.jobhunter.repository.CompanyRepository;
 
 @Service
@@ -16,4 +20,30 @@ public class CompanyService {
     public Company handleCreateCompany(Company c) {
         return this.companyRepository.save(c);
     }
+
+    public void handleDeleteCompany(Long id) {
+        this.companyRepository.deleteById(id);
+    }
+
+    public Company fetchCompanyById(Long id) {
+        return this.companyRepository.findById(id).orElse(null);
+    }
+
+    public List<Company> handleGetCompany() {
+        return this.companyRepository.findAll();
+    }
+
+    public Company handleUpdateCompany(Company c) {
+        Optional<Company> companyOptional = this.companyRepository.findById(c.getId());
+        if (companyOptional.isPresent()) {
+            Company currentCompany = companyOptional.get();
+            currentCompany.setLogo(c.getLogo());
+            currentCompany.setName(c.getName());
+            currentCompany.setDescription(c.getDescription());
+            currentCompany.setAddress(c.getAddress());
+            return this.companyRepository.save(currentCompany);
+        }
+        return null;
+    }
+
 }
