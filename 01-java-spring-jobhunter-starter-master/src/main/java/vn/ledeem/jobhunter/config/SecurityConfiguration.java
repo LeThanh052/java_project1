0b18746b934a53df7e91f9e6d4eec67915e57cd6
LeThanh052 <thanhle050204@gmail.com>
2025-11-06 19:38:31 +0700

    New refresh token

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/config/SecurityConfiguration.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/config/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration {
                         // prettier-ignore
                         authz
                                 .requestMatchers("/", "/login").permitAll()
-                                .requestMatchers("/", "/api/v1/auth/login").permitAll()
+                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
                                 .anyRequest().authenticated())
                 // .securityMatcher("/users/**")
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         grantedAuthoritiesConverter.setAuthorityPrefix("");
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("user");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("permission");
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/AuthController.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -17,10 +18,12 @@ import vn.ledeem.jobhunter.domain.dto.ResLoginDTO;
 import vn.ledeem.jobhunter.service.UserService;
 import vn.ledeem.jobhunter.ultil.SecurityUtil;
 import vn.ledeem.jobhunter.ultil.annotation.ApiMessage;
+import vn.ledeem.jobhunter.ultil.error.IdInvalidException;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,7 +52,7 @@ public class AuthController {
                                 .authenticate(authenticationToken);
 
                 // Tạo token và trả về cho người dùng
-
+                // Set thông tin authentication(người dùng) vào Security Context
                 SecurityContextHolder.getContext().setAuthentication(authentication);
 
                 ResLoginDTO res = new ResLoginDTO();
@@ -63,7 +66,7 @@ public class AuthController {
                         res.setUser(userLogin);
                 }
 
-                String access_token = this.securityUtil.createAccessToken(authentication, res.getUser());
+                String access_token = this.securityUtil.createAccessToken(authentication.getName(), res.getUser());
 
                 res.setAccessToken(access_token);
 
@@ -98,6 +101,53 @@ public class AuthController {
                         userLogin.setName(currentUserDB.getName());
                 }
                 return ResponseEntity.ok().body(userLogin);
+        }
+
+        @GetMapping("/auth/refresh")
+        @ApiMessage("Get User by refresh token")
+        public ResponseEntity<ResLoginDTO> getRefreshToken(
+                        @CookieValue(name = "refresh_token", required = false) String refreshToken)
+                        throws IdInvalidException {
+                // check valid refresh token
+                Jwt decodeJwt = this.securityUtil.checkValidRefreshToken(refreshToken);
+                String email = decodeJwt.getSubject();
+
+                // check refresh token and email match db
+                User currentUser = this.userService.getUserByRefreshTokenAndEmail(refreshToken, email);
+                if (currentUser == null) {
+                        throw new IdInvalidException("Refresh token is invalid");
+                }
+
+                ResLoginDTO res = new ResLoginDTO();
+                User currentUserDB = this.userService.handleGetUserByUsername(email);
+
+                if (currentUserDB != null) {
+                        ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+                                        currentUserDB.getId(),
+                                        currentUserDB.getEmail(),
+                                        currentUserDB.getName());
+                        res.setUser(userLogin);
+                }
+
+                String access_token = this.securityUtil.createAccessToken(email, res.getUser());
+
+                res.setAccessToken(access_token);
+
+                // Create Refresh Token
+                String new_refresh_token = this.securityUtil.createRefreshToken(email, res);
+
+                // Update refresh token to DB
+                this.userService.updateTokenUser(new_refresh_token, email);
+
+                ResponseCookie resCookie = ResponseCookie
+                                .from("refresh_token", new_refresh_token)
+                                .httpOnly(true)
+                                .path("/")
+                                .maxAge(7 * 24 * 60 * 60) // 7 days
+                                .build();
+                return ResponseEntity.ok()
+                                .header(HttpHeaders.SET_COOKIE, resCookie.toString())
+                                .body(res);
         }
 
 }
