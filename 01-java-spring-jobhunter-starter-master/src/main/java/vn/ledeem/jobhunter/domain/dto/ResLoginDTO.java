556435e5c52a4068433ed03a2f07db8b8dde34ec
LeThanh052 <thanhle050204@gmail.com>
2025-11-07 13:17:36 +0700

    config giao diện

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/domain/dto/ResLoginDTO.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/domain/dto/ResLoginDTO.java
@@ -1,5 +1,7 @@
 package vn.ledeem.jobhunter.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResLoginDTO {
+    @JsonProperty("access_token")
     private String accessToken;
 
     private UserLogin user;
@@ -22,6 +25,14 @@ public class ResLoginDTO {
         private long id;
         private String email;
         private String name;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserGetAccount {
+        private UserLogin user;
     }
 
 }
