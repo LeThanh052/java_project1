8eb08493eeccbc67e7d7116309affc428c04399b
LeThanh052 <thanhle050204@gmail.com>
2026-04-01 17:29:05 +0700

    fix permission

---
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/config/PermissionInterceptor.java
@@ -0,0 +1,64 @@
+package vn.ledeem.jobhunter.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.ledeem.jobhunter.domain.Permission;
+import vn.ledeem.jobhunter.domain.Role;
+import vn.ledeem.jobhunter.domain.User;
+import vn.ledeem.jobhunter.service.UserService;
+import vn.ledeem.jobhunter.ultil.SecurityUtil;
+import vn.ledeem.jobhunter.ultil.error.IdInvalidException;
+
+public class PermissionInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    @Transactional
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response, Object handler)
+            throws Exception {
+        String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        System.out.println(">>> RUN preHandle");
+        System.out.println(">>> path= " + path);
+        System.out.println(">>> httpMethod= " + httpMethod);
+        System.out.println(">>> requestURI= " + requestURI);
+
+        // check permission
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        if (email != null && !email.isEmpty()) {
+            User user = this.userService.handleGetUserByUsername(email);
+            if (user != null) {
+                Role role = user.getRole();
+                if (role != null) {
+                    List<Permission> permissions = role.getPermissions();
+                    boolean isAllow = permissions.stream()
+                            .anyMatch(item -> item.getApiPath().equals(path) && item.getMethod().equals(httpMethod));
+
+                    if (isAllow == false) {
+                        throw new IdInvalidException("Ban khong co quyen truy cap trang endpoint này");
+                    } else {
+                        throw new IdInvalidException("Ban khong co quyen truy cap trang endpoint này");
+
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
