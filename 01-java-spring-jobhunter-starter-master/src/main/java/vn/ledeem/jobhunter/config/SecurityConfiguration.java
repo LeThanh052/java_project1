2d34ae4667596f14198baf2738194dec49ac5119
LeThanh052 <thanhle050204@gmail.com>
2026-04-01 16:32:26 +0700

    fake Data and phân trang

--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/config/SecurityConfiguration.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -61,6 +62,9 @@ public class SecurityConfiguration {
                         authz
                                 .requestMatchers(whiteList).permitAll()
                                 .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/companies").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/skills").permitAll()
                                 .anyRequest().authenticated())
                 // .securityMatcher("/users/**")
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
--- 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/ResumeController.java
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/ledeem/jobhunter/controller/ResumeController.java
@@ -1,6 +1,8 @@
 package vn.ledeem.jobhunter.controller;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,13 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkraft.springfilter.boot.Filter;
+import com.turkraft.springfilter.builder.FilterBuilder;
+import com.turkraft.springfilter.converter.FilterSpecificationConverter;
+
 import jakarta.validation.Valid;
+import vn.ledeem.jobhunter.domain.Company;
+import vn.ledeem.jobhunter.domain.Job;
 import vn.ledeem.jobhunter.domain.Resume;
+import vn.ledeem.jobhunter.domain.User;
 import vn.ledeem.jobhunter.domain.response.ResultPaginationDTO;
 import vn.ledeem.jobhunter.domain.response.resume.ResCreateResumeDTO;
 import vn.ledeem.jobhunter.domain.response.resume.ResFetchResumeDTO;
 import vn.ledeem.jobhunter.domain.response.resume.ResUpdateResumeDTO;
 import vn.ledeem.jobhunter.service.ResumeService;
+import vn.ledeem.jobhunter.service.UserService;
+import vn.ledeem.jobhunter.ultil.SecurityUtil;
 import vn.ledeem.jobhunter.ultil.annotation.ApiMessage;
 import vn.ledeem.jobhunter.ultil.error.IdInvalidException;
 
@@ -31,9 +41,17 @@ import vn.ledeem.jobhunter.ultil.error.IdInvalidException;
 @RequestMapping("/api/v1")
 public class ResumeController {
     private final ResumeService resumeService;
+    private final UserService userService;
 
-    public ResumeController(ResumeService resumeService) {
+    private final FilterBuilder filterBuilder;
+    private final FilterSpecificationConverter filterSpecificationConverter;
+
+    public ResumeController(ResumeService resumeService, UserService userService, FilterBuilder filterBuilder,
+            FilterSpecificationConverter filterSpecificationConverter) {
         this.resumeService = resumeService;
+        this.userService = userService;
+        this.filterBuilder = filterBuilder;
+        this.filterSpecificationConverter = filterSpecificationConverter;
     }
 
     @PostMapping("/resumes")
@@ -92,7 +110,29 @@ public class ResumeController {
             @Filter Specification<Resume> spec,
             Pageable pageable) {
 
-        return ResponseEntity.ok().body(this.resumeService.fetchAllResume(spec, pageable));
+        List<Long> arrJobIds = null;
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        User currentUser = this.userService.handleGetUserByUsername(email);
+        if (currentUser != null) {
+            Company userCompany = currentUser.getCompany();
+            if (userCompany != null) {
+                List<Job> companyJobs = userCompany.getJobs();
+                if (companyJobs != null && companyJobs.size() > 0) {
+                    arrJobIds = companyJobs.stream().map(x -> x.getId())
+                            .collect(Collectors.toList());
+                }
+            }
+        }
+        Specification<Resume> jobInSpec = filterSpecificationConverter.convert(filterBuilder.field("job")
+                .in(filterBuilder.input(arrJobIds)).get());
+
+        Specification<Resume> finalSpec = jobInSpec.and(spec);
+
+        return ResponseEntity.ok().body(this.resumeService.fetchAllResume(finalSpec, pageable));
+
     }
 
     @PostMapping("/resumes/by-user")
